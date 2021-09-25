9d9baa1a55a8a6bcabff63fcc7a8bdbefc0f3c04
******* <bankdim2006@gmail.com>
2021-09-25 14:44:20 +0200

    Update manager settings

---
+++ Assets/Scripts/Events/UptateManager.cs
@@ -0,0 +1,25 @@
+﻿using Events;
+using UnityEngine;
+
+namespace Game
+{
+    public class UptateManager : MonoBehaviour
+    {
+        [SerializeField]
+            private ScriptableEvent _updateEvent;
+
+        [SerializeField]
+        private ScriptableEvent _fixedEvent;
+
+        private void Update()
+        {
+            _updateEvent.Dispatch();
+        }
+
+        private void FixedUpdate()
+        {
+            _fixedEvent.Dispatch();
+        }
+    }
+
+}
