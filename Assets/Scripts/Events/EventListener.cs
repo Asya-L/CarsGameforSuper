19e64bf164c6607c5b18d39a0700058ffb062652
******* <bankdim2006@gmail.com>
2021-09-25 14:34:51 +0200

    Event settings

---
+++ Assets/Scripts/Events/EventListener.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events
+{
+
+
+
+
+    public class EventListener : MonoBehaviour
+    {
+
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+
+        public event Action OnEventHappend = delegate { };
+
+        private void OnEnable()
+        {
+            _someEvent.AddListener(EventHappend);
+        }
+
+        private void OnEnable()
+        {
+            _someEvent.AddListener(EventHappened);
+        }
+
+        private void OnDisable()
+        {
+            _someEvent.RemoveListener(EventHappend);
+        }
+
+        private void EventHappend()
+        {
+
+            OnEventHappend.Invoke();
+        }
+
+
+        public void Dispatch()
+        {
+            _someEvent.Dispatch();
+        }
+
+    }
+
+}
+
